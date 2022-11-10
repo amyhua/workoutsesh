d7caf386074f9bd9817c3994ef34aa0efc03fe78
Amy Hua <amyhua@falconx.io>
2022-11-09 20:04:42 -0600

    Add barbell loading screen. Add signin and signup form components.

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,4 +1,6 @@
+import classNames from "classnames";
 import Image from "next/image"
+import { useState } from "react";
 import Layout from "../components/Layout"
 
 const AppleIconSvg = () => (
@@ -7,10 +9,45 @@ const AppleIconSvg = () => (
   </svg>
 )
 
+const LoadingPage = ({ active, message }: { active?: boolean; message: string; }) => (
+  <div className={classNames(
+    "flex justify-center absolute left-0 top-0 right-0 bottom-0 text-center h-full",
+    {
+      "hidden": !active
+    }
+  )}>
+    <div className="z-10 bg-white absolute top-0 bottom-0 left-0 right-0 opacity-95" />
+    <div className="z-20 h-full flex justify-center flex-col">
+      <Image className="mx-auto animate-bounce w-[14] h-[14]" alt="Loading Spinner" src={require('../components/barbell-circle.svg')} />
+      <div className="mt-5 text-center text-black">
+        <span className="uppercase tracking-widest italic font-bold text-blue-700">
+          {message || 'Just a second'}
+        </span>
+      </div>
+    </div>
+  </div>
+)
+
+enum SigninMethod {
+  Apple = 'Apple',
+  Email = 'Email',
+  Login = 'Login',
+  Create = 'Signup'
+}
+
 const SigninPage = () => {
+  const [loading, setLoading] = useState(false);
+  const [focusedMethod, setFocusedMethod] = useState<SigninMethod>();
+  const signInWithEmail = () => {
+    setFocusedMethod(SigninMethod.Email)
+  }
+  const startCreateAccount = () => {
+    setFocusedMethod(SigninMethod.Create)
+  }
   return (
     <Layout title="Sign in | Workout Sesh" background="white">
-      <main className="relative max-w-lg mx-auto min-h-[100vh] p-5">
+      <main className="relative max-w-lg mx-auto h-full min-h-[800px] p-5">
+        <LoadingPage active={loading} message="Signing you in" />
         <header className="mt-10">
           <Image
             className="mx-auto"
@@ -31,11 +68,70 @@ const SigninPage = () => {
           <button className="text-base p-3 w-full bg-black text-white rounded-lg">
             <AppleIconSvg /> Sign in with Apple
           </button>
-          <button className="text-base mt-3 p-3 w-full text-white bg-blue-500 rounded-lg">
-            Sign Up with Email
+          <button
+            onClick={signInWithEmail}
+            className={classNames(
+              "text-base mt-3 p-3 w-full text-black bg-brightGreen rounded-lg",
+              {
+                "hidden": focusedMethod === SigninMethod.Email
+              }
+            )}>
+            Sign in with Email
           </button>
-          <button className="text-base mt-3 p-3 w-full rounded-lg text-slate-500">
-            Log In
+          <form className={classNames({
+            "hidden": !(
+              focusedMethod === SigninMethod.Email ||
+              focusedMethod === SigninMethod.Create
+            )
+          })} onSubmit={() => {}}>
+            <input
+              className="rounded-lg w-full p-3 mt-3 bg-slate-200"
+              type="email"
+              name="email"
+              placeholder="Email Address"
+            />
+            <div className="flex">
+              <input
+                className={classNames(
+                  "p-3 mt-3 bg-slate-200",
+                  "flex-1 rounded-tl-lg rounded-bl-lg",
+                  {
+                    "border-r border-r-slate-300": focusedMethod === SigninMethod.Create
+                  }
+                )}
+                type="password"
+                name="password"
+                placeholder="Password"
+              />
+              <input
+                className={classNames(
+                  "p-3 mt-3 bg-slate-200",
+                  {
+                    "hidden": focusedMethod === SigninMethod.Email,
+                    // "rounded-lg": focusedMethod === SigninMethod.Create,
+                    "flex-1 rounded-tl-lg rounded-bl-lg": focusedMethod === SigninMethod.Email
+                  }
+                )}
+                type="password"
+                name="confirmPassword"
+                placeholder="Confirm Password"
+              />
+              <button
+                type="submit"
+                className="p-3 mt-3 rounded-tr-lg rounded-br-lg font-bold bg-brightGreen text-black"
+              >
+                →
+              </button>
+            </div>
+          </form>
+          <button onClick={startCreateAccount}
+          className={classNames(
+            "text-base mt-3 p-3 w-full rounded-lg text-slate-500",
+            {
+              "hidden": focusedMethod === SigninMethod.Create
+            }
+          )}>
+            Create a New Account
           </button>
         </div>
       </main>
