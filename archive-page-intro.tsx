253798a5f563c4e1228d2f5835a1aca43447c5e7
Amy Hua <amyhua@falconx.io>
2022-12-30 17:23:24 -0800

    Fix imports

--- archive-page-intro.tsx
+++ archive-page-intro.tsx
@@ -1,8 +1,8 @@
 import classNames from "classnames";
 import { useRouter } from "next/router";
 import { useRef, useEffect, useState, useLayoutEffect } from "react";
-import Layout from "../components/Layout";
-import Logo from "../components/Logo";
+import Layout from "./components/Layout";
+import Logo from "./components/Logo";
 
 const AppChatAvatarSvg = ({ size=22, className }: { size?: number; className: string; }) => (
   <svg className={className} width={size} height={size} viewBox="0 0 22 22" fill="none" xmlns="http://www.w3.org/2000/svg">
