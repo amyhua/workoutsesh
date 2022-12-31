03984f7d82b12d6b49b0f1f16299a802edbf8a0f
Amy Hua <amyhua@falconx.io>
2022-12-30 17:31:01 -0800

    Remove dupe api route

--- pages/api/workout/index.ts
+++ pages/api/workout/index.ts
@@ -36,6 +36,81 @@ async function workoutRoute(req: NextApiRequest, res: NextApiResponse<any>) {
         }
       }
       break;
+    case 'PUT':
+      req.body = req.body ? JSON.parse(req.body) : {}
+      if (!req.body ||  !req.body.id) {
+        throw new Error('Missing workout ID as body field [id]')
+      }
+      if (typeof session.user.email !== 'string') {
+        throw new Error('Missing logged in user')
+      }
+      const workoutExercises = req.body.exercises || [];
+      workoutExercises.forEach(async (exc: Exercise) => {
+        await prisma.exercise.upsert({
+          where: {
+            id: exc.id ? Number(exc.id) : 0,
+          },
+          update: {
+            ...exc,
+            workout: undefined,
+            workoutId: Number(req.body.id),
+          },
+          create: {
+            ...exc,
+            workout: undefined,
+            workoutId: Number(req.body.id),
+          }
+        });
+      });
+      const workout = await prisma.workout.update({
+        where: {
+          id: Number(req.body.id),
+        },
+        data: {
+          ...req.body,
+          exercises: undefined,
+        },
+      });
+      res.json(workout);
+      break;
+    case 'POST':
+      const {
+        name,
+        description,
+        exercises,
+      } = JSON.parse(req.body || {});
+      if (session.user.email) {
+        const workout = await prisma.workout.create({
+          data: {
+            name,
+            description,
+            slug: name.toLowerCase().replace(/\s+/g, '-'),
+            userEmail: session.user.email,
+          }
+        })
+        const createdExercises = [] as Exercise[];
+        try {
+          exercises
+            .forEach(async (exercise: Exercise, i: number) => {
+            const exc = await prisma.exercise.create({
+              data: {
+                ...exercise,
+                workoutId: workout.id
+              }
+            })
+            createdExercises.push(exc);
+          });
+        } catch(err) {
+          return res.status(500).json({
+            error: `${exercises.length} exercises were meant to be created, but ${createdExercises.length} created.`
+          });
+        }
+        return res.json({
+          ...workout,
+          exercises: createdExercises,
+        });
+      }
+      break;
     default:
       res.setHeader('Allow', ['GET'])
       res.status(405).end(`Method ${req.method} Not Allowed`)
