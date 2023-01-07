05afa49ca412c59009ac86f4b381cb488dcab0e7
Amy Hua <amyhua@falconx.io>
2023-01-06 17:22:56 -0800

    Allow unsaved sesh workout player

---
+++ pages/w/[slug].tsx
@@ -0,0 +1,51 @@
+import { prisma } from '../../lib/prismadb'
+import WorkoutPlayer from '../../components/WorkoutPlayer'
+
+function WorkoutShareBySlugPage({
+  workout,
+  error,
+  saveCurrentInterval,
+}: any) {
+  return (
+    <WorkoutPlayer
+      workout={workout}
+      error={error}
+      saveCurrentInterval={saveCurrentInterval}
+      isUnsavedSesh={true}
+    />
+  );
+}
+
+export async function getServerSideProps(context: any) {
+  const { slug } = context.query;
+  try {
+    const workout = await prisma.workout.findFirstOrThrow({
+      where: {
+        slug: slug,
+      },
+      include: {
+        exercises: true,
+        seshes: {
+          where: {
+            finishedAt: null,
+          }
+        }
+      },
+    });
+    return {
+      props : {
+        workout: workout ? JSON.stringify(workout) : null,
+        params: context.params
+      }
+    }
+    // TODO: handle not logged in user
+  } catch(error) {
+    return {
+      props : {
+        error: error ? JSON.stringify(error) : null,
+      }
+    }
+  }
+}
+
+export default WorkoutShareBySlugPage;
